3742a2850c6b9b65becd6f309af8dfab07a437ad
Eduardo Pereira <eduardo.pereira@compasso.com.br>
2025-02-17 15:29:06 -0300

    ajuste nas metadatas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,38 @@
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 import { Providers } from "@/providers/Providers";
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from "next/head";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Eduardo Pereira - Portifólio",
-  description: "Portifólio do Eduardo Pereira",
-};
+export const metadata = {
+  title: "Eduardo Silva Pereira - Desenvolvedor Web e Mobile",
+  authors: ["Eduardo Silva Pereira"],
+  description:
+    "Desenvolvedor especializado em React, React Native, Next.js e Node.js. Construindo aplicações performáticas e escaláveis.",
+  keywords: [
+    "React",
+    "Next.js",
+    "TypeScript",
+    "Desenvolvedor Web",
+    "Node.js",
+    "React Native",
+  ],
+  robots: "index, follow",
+  openGraph: {
+    title: "Eduardo Silva Pereira - Desenvolvedor Web e Mobile",
+    description:
+      "Desenvolvedor especializado em aplicações escaláveis e performáticas.",
+    url: "https://eduardosilvapereira.com.br",
+    images: {
+      url: "/assets/eduardo-silva-pereira-thumb.jpg",
+      alt: "Eduardo Silva Pereira - Desenvolvedor Web e Mobile",
+    },
+  },
+} as Metadata;
 
 export default function RootLayout({
   children,
@@ -20,33 +41,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <Head>
-        <title>Eduardo Silva Pereira - Desenvolvedor Web e Mobile</title>
-        <meta
-          name="description"
-          content="Desenvolvedor especializado em React, React Native, Next.js e Node.js. Construindo aplicações performáticas e escaláveis."
-        />
-        <meta
-          name="keywords"
-          content="React, Next.js, TypeScript, Desenvolvedor Web, Node.js, React Native"
-        />
-        <meta name="author" content="Eduardo Silva Pereira" />
-        <meta
-          property="og:title"
-          content="Eduardo Silva Pereira - Desenvolvedor Web e Mobile"
-        />
-        <meta
-          property="og:description"
-          content="Desenvolvedor especializado em aplicações escaláveis e performáticas."
-        />
-        <meta
-          property="og:image"
-          content="/assets/eduardo-silva-pereira-thumb.jpg"
-        />
-        <meta property="og:url" content="https://eduardosilvapereira.com.br" />
-        <meta name="robots" content="index, follow" />
+      <body
+        className={`min-h-screen mx-auto max-w-screen-lg flex flex-col bg-white dark:bg-gray-900 ${inter.className}`}
+      >
+        <Providers>
+          <Navbar />
+          <main className="flex flex-col flex-1 max-w-screen-lg w-full">
+            {children}
+          </main>
+          <Footer />
+        </Providers>
 
-        <script
+        <Script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
@@ -63,11 +69,13 @@ export default function RootLayout({
           }}
         />
 
-        <script
+        <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-LS6R8STSND"
-        ></script>
-        <script
+        ></Script>
+
+        <Script
+          type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
@@ -76,18 +84,7 @@ export default function RootLayout({
             gtag('config', 'G-LS6R8STSND');
           `,
           }}
-        ></script>
-      </Head>
-      <body
-        className={`min-h-screen mx-auto max-w-screen-lg flex flex-col bg-white dark:bg-gray-900 ${inter.className}`}
-      >
-        <Providers>
-          <Navbar />
-          <main className="flex flex-col flex-1 max-w-screen-lg w-full">
-            {children}
-          </main>
-          <Footer />
-        </Providers>
+        ></Script>
       </body>
     </html>
   );
